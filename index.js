b1f0d0a7527eb5e7d24168e45517dfb0ee3d0ace
MOHD ASEEM AKRAM <113539818+Aseemakram19@users.noreply.github.com>
2023-11-30 20:36:25 +0530

    Update index.js
    update welome with port 3000

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('A Monk in Cloud'); //write a response to the client
+  res.write('welcome to cloud aseem youtube channel'); //write a response to the client
   res.end(); //end the response
-}).listen(80); //the server object listens on port 80
+}).listen(3000); //the server object listens on port  3000
+
