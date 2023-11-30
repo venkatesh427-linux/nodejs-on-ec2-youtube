e519a2e7a202c1f7f0000f98e979baab07389ab0
MOHD ASEEM AKRAM <113539818+Aseemakram19@users.noreply.github.com>
2023-11-30 20:41:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('welcome to cloud aseem youtube channel'); //write a response to the client
+  res.write('A Monk in Cloud'); //write a response to the client
   res.end(); //end the response
-}).listen(3000); //the server object listens on port  3000 node
+}).listen(80); //the server object listens on port 80
 
